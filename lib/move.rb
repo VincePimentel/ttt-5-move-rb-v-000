ea6d9f121b8ca2af21fc388ffe09a1a28d5c8ce4
Vince Andrew Pimentel <contact@vincep.me>
2018-10-03 03:37:38 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,8 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  user_input.to_i - 1
+  index = user_input.to_i - 1
+  puts index
 end
 
 def move(array, index, character = "X")
